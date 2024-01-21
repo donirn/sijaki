9fd0b94e93f8f41afbf23d30de5ac7bf6293f995
Doni Ramadhan <doniramadhan@gmail.com>
2024-01-21 21:46:06 +0700

    upgrade jquery

--- source/application/views/header.php
+++ source/application/views/header.php
@@ -1,7 +1,7 @@
 <html>
 <head>
 	<title>SIJAKI</title>
-	<script type="text/javascript" src="<?php echo base_url();?>js/jquery-1.9.1.min.js"></script>
+	<script type="text/javascript" src="<?php echo base_url();?>js/jquery-3.7.1.min.js"></script>
 	<script type="text/javascript" src="<?php echo base_url();?>js/bootstrap.min.js"></script>
 	<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>css/bootstrap.min.css">
 	<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>css/style.css">
